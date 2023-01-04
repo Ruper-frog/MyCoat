a95c7cc6c0d400847e185f5cafec29eb0c3684fb
Ruper-frog <ruepr120406@gmail.com>
2023-01-04 19:54:23 +0200

    Added the Bull's Eye Game

--- MyCoat/Program.cs
+++ MyCoat/Program.cs
@@ -7,7 +7,6 @@ namespace MyCoat
         static void Main(string[] args)
         {
             gameStart();
-            game();
         }
         //Decoration Department, Cheat Team, 
         static void game()
@@ -107,6 +106,78 @@ namespace MyCoat
 
             } while (keyinfo.Key != ConsoleKey.Escape);
         }
+        //BullsEye Game
+        static void BullsEye()
+        {
+            ConsoleKeyInfo keyinfo;
+            char s = '*';
+            bool penDown = true;
+            int x = 0, y = 0;
+
+            // MENU //
+            Console.SetCursorPosition(1, 20);
+            Console.WriteLine("____________");
+            Console.WriteLine("C- Clear Screen, P- Change Pen, L- Change Color, U- Pen Up, D- Pen Down, Escape - Exit");
+            Console.SetCursorPosition(1, 1);
+
+            string heart = "▓";
+            int l, c;
+
+            Random rnd = new Random();
+            l = rnd.Next(0, 20);
+            c = rnd.Next(0, 20);
+
+            Console.SetCursorPosition(l, c);
+            Console.Write(heart);
+
+            keyinfo = Console.ReadKey();
+
+            while (keyinfo.Key != ConsoleKey.Escape)
+            {
+                keyinfo = Console.ReadKey();
+
+                // Move corsore
+                if (keyinfo.Key == ConsoleKey.RightArrow) x++;
+                if (keyinfo.Key == ConsoleKey.LeftArrow) x--;
+                if (keyinfo.Key == ConsoleKey.DownArrow) y++;
+                if (keyinfo.Key == ConsoleKey.UpArrow) y--;
+
+                // Other menu keys
+
+                if (keyinfo.Key == ConsoleKey.U) penDown = false;
+                if (keyinfo.Key == ConsoleKey.D) penDown = true;
+                if (keyinfo.Key == ConsoleKey.R) Console.BackgroundColor = ConsoleColor.Red;
+                if (keyinfo.Key == ConsoleKey.W) Console.BackgroundColor = ConsoleColor.White;
+                if (x == c && y == l)
+                {
+                    Console.Clear();
+                    Console.SetCursorPosition(50, 10);
+                    Console.WriteLine("YOU WON!!!");
+                }
+                if (keyinfo.Key == ConsoleKey.C)
+                {
+                    Console.Clear();
+                    // MENU //
+                    Console.SetCursorPosition(1, 20);
+                    Console.WriteLine("____________");
+                    Console.WriteLine("C- Clear Screen, P- Change Pen, L- Change Color, U- Pen Up, D- Pen Down, Escape - Exit");
+                    Console.SetCursorPosition(1, 1);
+                }
+
+                if (keyinfo.Key == ConsoleKey.P)
+                {
+                    if (s == '*')
+                        s = '+';
+                    else
+                        s = '*';
+                }
+
+                // Set Cursoe Position and pain if pen is down.
+
+                Console.SetCursorPosition(x, y);
+                if (penDown) Console.Write(s);
+            }
+        }
         //Decoration Department
         static void trial()
         {
@@ -193,7 +264,18 @@ namespace MyCoat
 
             } while (keyinfo.Key != ConsoleKey.Enter);
 
+            //Some adjustments
+            Console.WriteLine("\n1 - Draw Game\n2 - BullsEye");
+            int Choice = int.Parse(Console.ReadLine());
+
             Console.Clear();
+
+            if (Choice == 1)
+                game();
+
+            else if (Choice == 2)
+                BullsEye();
+
         }
         //Cheat Team
         private static int NewMethod1()
